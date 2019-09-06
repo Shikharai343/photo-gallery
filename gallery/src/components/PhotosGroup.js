3ff20d9e6293af84382a1f68d6730e225cd99214
raiShikha <shikharai@nineleaps.com>
2019-09-06 10:27:40 +0530

    basic search ui setup

--- gallery/src/components/PhotosGroup.js
+++ gallery/src/components/PhotosGroup.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const PhotosGroup = () => {
-	return <div>Photos Group</div>
+	return (
+		<div className="ui clearing segment">
+			<div className="ui right floated segment">
+				<div className="ui search">
+					<div className="ui icon input">
+						<input className="prompt" type="text" placeholder="Common passwords..."/>
+						<i className="search icon"/>
+					</div>
+					<div className="results"/>
+				</div>
+			</div>
+		</div>
+	)
 };
 
 export default PhotosGroup;
